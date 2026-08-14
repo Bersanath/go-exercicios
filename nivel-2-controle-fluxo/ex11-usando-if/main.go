f47d57ef47f5652f5c12cafe21ca8dd41bcd65c0
Bersanath <eduardofenix102@gmail.com>
2026-08-14 22:38:34 +0100

    ex11: if com statement de inicialização
    - Variável com scope limitado ao bloco if/else
    - Evita poluição da função com variáveis temporárias
    - Padrão idiomático: if err := f(); err != nil

---
+++ nivel-2-controle-fluxo/ex11-usando-if/main.go
@@ -0,0 +1,14 @@
+/*Usa if com statement de inicialização.*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	if idade := 22; idade > 18 {
+
+		fmt.Printf("Você tem: %d anos de idade", idade)
+	}
+
+}
