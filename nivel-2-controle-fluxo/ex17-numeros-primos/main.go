d965797efe027ce3b37cb4efcd8e06d5426900f9
Bersanath <eduardofenix102@gmail.com>
2026-08-20 00:27:14 +0100

    ex17: Verificando se um número é primo

--- nivel-2-controle-fluxo/ex17-numeros-primos/main.go
+++ nivel-2-controle-fluxo/ex17-numeros-primos/main.go
@@ -7,27 +7,27 @@ import (
 	"math"
 )
 
-func main(){
+func main() {
 
 	var num int
 
 	fmt.Println("Digite um Número: ")
-	
-	fmt.Scan(&num)  
+
+	fmt.Scan(&num)
 
 	primo := true // assume que é primo
 
 	if num > 1 {
 
-		for i := 2; i <= int(math.Sqrt(float64(num))); i++{
+		for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
 
-			if num % i == 0 {
+			if num%i == 0 {
 
-				primo = false  // achou um divisor, não é primo
-				break  // sai do for
+				primo = false // achou um divisor, não é primo
+				break         // sai do for
 			}
 		}
-	}else {
+	} else {
 
 		primo = false // números menores ou iguais a 1 não são primos
 	}
@@ -35,7 +35,7 @@ func main(){
 
 		fmt.Printf("O número: %d é Primo!", num)
 
-	}else{
+	} else {
 
 		fmt.Printf("O número: %d Não é Primo!", num)
 	}
